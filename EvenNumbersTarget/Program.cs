d8694205376d8e5f560bd9df2cefaf0c0b3f17b2
mosamircs <mosamircs@gmail.com>
2021-05-13 02:17:30 +0200

    Add project files.

---
+++ EvenNumbersTarget/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EvenNumbersTarget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Even Numbers Target app");
+        }
+    }
+}
