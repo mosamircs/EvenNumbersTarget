15ade5b6d562fae802d3686adda17b1fa3bee4b9
mosamircs <mosamircs@gmail.com>
2021-05-13 02:23:23 +0200

    making it using for loop

--- EvenNumbersTarget/Program.cs
+++ EvenNumbersTarget/Program.cs
@@ -7,6 +7,21 @@ namespace EvenNumbersTarget
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Even Numbers Target app");
+            
+            Console.WriteLine("Enter your first Number:");
+            int FirstNumber = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Enter your Target Number:");
+            int TargettNumber = int.Parse(Console.ReadLine());
+
+            for(int i = FirstNumber;i<=TargettNumber;i++)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write(i+" ");
+                }
+            }
+            Console.WriteLine();
         }
     }
 }
